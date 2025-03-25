344f788f746e6c0dbb5af2d906a2b0ad33d17761
Lawrence Angelo Noriesta <40698971+lanoriesta@users.noreply.github.com>
2025-03-25 21:43:04 +0800

    change image path from project JS

--- src/projects.js
+++ src/projects.js
@@ -1,16 +1,21 @@
+import ThumbnailOne from "./assets/work thumbnail/2.png";
+import ThumbnailTwo from "./assets/work thumbnail/3.png";
+import ThumbnailThree from "./assets/work thumbnail/4.png";
+import ThumbnailFour from "./assets/work thumbnail/5.png";
+
 const projects = [
   {
     id: 1,
     name: "Product Store",
     description: "A simple shopping cart app",
-    image: "src/assets/work thumbnail/2.png",
+    image: ThumbnailOne,
     img_link: "https://product-app-1igi.onrender.com/",
   },
   {
     id: 2,
     name: "Gawad Kalinga USA",
     description: "A simple web design",
-    image: "src/assets/work thumbnail/3.png",
+    image: ThumbnailTwo,
     img_link:
       "https://www.figma.com/proto/cnS5JDbcBBRXVWSr7WAO0F/MagisProject_GawadKalinga?page-id=0%3A1&node-id=1-2&scaling=scale-down-width&content-scaling=fixed&t=olQQXyyv2iJfWThl-1",
   },
@@ -18,14 +23,14 @@ const projects = [
     id: 3,
     name: "PortGiving Tuesday Philippines",
     description: "A simple web design",
-    image: "src/assets/work thumbnail/4.png",
+    image: ThumbnailThree,
     img_link: "https://giving-tuesday-project.onrender.com/",
   },
   {
     id: 4,
     name: "Magis Solutions",
     description: "A simple web design",
-    image: "src/assets/work thumbnail/5.png",
+    image: ThumbnailFour,
     img_link:
       "https://www.figma.com/proto/qtXBcv0xRZretxUr1aD6Qw/MagisSolution?page-id=0%3A1&node-id=1-2&viewport=744%2C101%2C0.1&t=ST399WsJZFnuRZUn-1&scaling=scale-down-width&content-scaling=fixed",
   },
