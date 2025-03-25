d811c9da8c53593376f35aa778c2a55864411cf2
Lawrence Angelo Noriesta <40698971+lanoriesta@users.noreply.github.com>
2025-03-25 22:43:01 +0800

    add content MainKeyVisualSection for test

--- src/components/MainKeyVisualSection.jsx
+++ src/components/MainKeyVisualSection.jsx
@@ -1,7 +1,19 @@
 import React from "react";
 
 const MainKeyVisualSection = () => {
-  return <div>MainKeyVisualSection</div>;
+  return (
+    <div className="flex flex-col justify-center items-center py-[150px]">
+      <div className="pb-5">
+        {/* <img
+          src={HeadShot}
+          alt="Lawrence Angelo Noriesta"
+          className="size-30 shrink-0 rounded-full contrast-[.9] brightness-100 shadow-lg"
+        /> */}
+      </div>
+      <h1>Hi, I'm Lawrence</h1>
+      <h3>Frontend Developer & Web Designer</h3>
+    </div>
+  );
 };
 
 export default MainKeyVisualSection;
